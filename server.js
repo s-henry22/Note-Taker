cefaa0a34927d4408e3addc25aa4d4b5dd346605
Spencer Henry <spencerhenry94@gmail.com>
2021-05-09 16:15:20 -0400

    Add Heroku

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const { notes } = require('./data/notes');
+
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.name) {
+        filteredResults = filteredResults.filter(note => note.name === query.name);
+    }
+    return filteredResults;
+}
+
+// Adding API route
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001.`);
+});
