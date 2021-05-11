3756dd805a171228835ee1b6cc82cf642be3740b
Spencer Henry <spencerhenry94@gmail.com>
2021-05-10 23:17:51 -0400

    created post requests

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 const { notes } = require('./data/notes');
 
@@ -16,6 +23,24 @@ function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id ===id) [0];
     return result;
 };
+
+// writes to notes.json
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+function validateNote (note) {
+    if (!note.name || typeof note.name !== 'string') {
+        return false;
+    }
+    return true;
+}
 
 // Adding API route
 app.get('/api/notes', (req, res) => {
@@ -34,6 +59,19 @@ app.get('/api/notes/:id', (req, res) => {
     } else {
         res.send(404);
     }  
+});
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 app.listen(PORT, () => {
